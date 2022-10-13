068266a3793d561cba229fcdcaea1c7d20b717b5
Henrique Serraglia <ricks@duck.com>
2022-10-12 22:05:58 -0300

    day 03 finished

--- src/components/Checkbox.tsx
+++ src/components/Checkbox.tsx
@@ -3,11 +3,14 @@ import { clsx } from 'clsx';
 import { Check } from 'phosphor-react';
 import { ReactNode } from 'react';
 
-export interface CheckboxProps {}
+export interface CheckboxProps extends CheckboxPrimitive.CheckboxProps {}
 
-export const Checkbox = ({}: CheckboxProps) => {
+export const Checkbox = (props: CheckboxProps) => {
 	return (
-		<CheckboxPrimitive.Root className="w-6 h-6 p-[2px] bg-gray-800 rounded">
+		<CheckboxPrimitive.Root
+			className="w-6 h-6 p-[2px] bg-gray-800 rounded"
+			{...props}
+		>
 			<CheckboxPrimitive.Indicator
 				asChild
 				children={<Check weight="bold" className="h-5 w-5 text-cyan-500" />}
